b2a5c19cf3e64a11935777bd699cd9131d3079d6
travisbrigman <travis1000@gmail.com>
2020-09-11 11:00:55 -0500

    initial commit

---
+++ src/components/HeyBurrito.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./HeyBurrito.css"
+
+export const HeyBurrito = () => (
+    <>
+        <h2>HeyBurrito!</h2>
+        <small>your hometown burrito shop since 2001</small>
+        <small><div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></small>
+    </>
+)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HeyBurrito } from './components/HeyBurrito';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HeyBurrito />
   </React.StrictMode>,
   document.getElementById('root')
 );
