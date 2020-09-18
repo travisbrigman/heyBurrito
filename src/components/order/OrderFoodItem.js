3d5b21e4fcfb22628407635517460f0cdd53fbde
travisbrigman <travis1000@gmail.com>
2020-09-18 14:03:12 -0500

    added more code

--- src/components/order/OrderFoodItem.js
+++ src/components/order/OrderFoodItem.js
@@ -2,20 +2,31 @@ import React from "react";
 
 
 
-export const FoodOrderItem = ({ foodItemObject, props, deleteOrder, foodDetails }) => {
+export const FoodOrderItem = ({ foodItemObject, props, deleteOrder, foodDetails, ingredients, foodItemIngredients }) => {
 
     const foodDetailObject = foodDetails.find(foodDetailObject => foodDetailObject.id === foodItemObject.detailId)
 
     const ItemIngredientList = () => {
-        
+        //this will give me an array with just FoodItemIngredient Objects that match the ID of the current FoodItem.
+        const ingredientIdList = foodItemIngredients.filter(ingredient => ingredient.foodItemId === foodItemObject.id)
+
+        //this should take that array and and for each object in that array, match it with a food ingredient.
+        const itemIngredientList = ingredientIdList.map(ingredientIdObject => {
+            return ingredients.find( ingredient => ingredient.id === ingredientIdObject.ingredientsId)
+
+        })
+        return itemIngredientList
+            
+            
     }
-  return (
+    return (
     <section className="foodOrderItem">
       <h3 className="foodOrderItem__name">{foodDetailObject.name}</h3>
       <div className="foodItem__Instructions">
         {foodItemObject.instructions}
       </div>
       <div className="foodOrderItem__Combo">{}</div>
+      <ItemIngredientList></ItemIngredientList>
 
       <button className="editFoodItem" onClick={() => props.push("/create")}>
         edit
@@ -26,3 +37,22 @@ export const FoodOrderItem = ({ foodItemObject, props, deleteOrder, foodDetails 
     </section>
   );
 };
+
+/*
+export const CustomerList = () => {
+    // This state changes when `getCustomers()` is invoked below
+    const { customers, getCustomers } = useContext(CustomerContext)
+
+    useEffect(() => {
+        getCustomers()
+    },[])
+
+    return (
+        <div className="customers">
+        {
+            customers.map(customerObject => <Customer key={customerObject.id} customer={customerObject} />)
+        }
+        </div>
+    )
+}
+*/
--- src/components/order/OrderList.js
+++ src/components/order/OrderList.js
@@ -1,29 +1,42 @@
-import React, { useContext, useEffect } from "react"
-import { OrderContext } from "./OrderProvider"
-import { FoodItemContext } from "../foodItem/FoodItemProvider"
-import { OrderFoodItem } from "./OrderFoodItem"
-import { FoodDetailContext } from "../foodItem/FoodDetailProvider"
-
-
+import React, { useContext, useEffect } from "react";
+import { OrderContext } from "./OrderProvider";
+import { FoodItemContext } from "../foodItem/FoodItemProvider";
+import { OrderFoodItem } from "./OrderFoodItem";
+import { FoodDetailContext } from "../foodItem/FoodDetailProvider";
+import { IngredientContext } from "../ingredients/IngredientProvider";
 
 export const OrderList = () => {
-    
-    const { orders, getOrders } = useContext(OrderContext)
-    const { foodItems, getFoodItems } = useContext(FoodItemContext)
-    const { foodDetails, getFoodDetails } = useContext(FoodDetailContext)
-    const { deleteOrder } = useContext(OrderContext)
+  const { orders, getOrders } = useContext(OrderContext);
+  const { foodItems, getFoodItems } = useContext(FoodItemContext);
+  const { foodDetails, getFoodDetails } = useContext(FoodDetailContext);
+  const { deleteOrder } = useContext(OrderContext);
+  const {
+    ingredients,
+    getIngredients,
+    foodItemIngredients,
+    getFoodItemIngredients,
+  } = useContext(IngredientContext);
 
-    useEffect(() => {
-        getOrders()
-        getFoodItems()
-        getFoodDetails()
-    },[])
+  useEffect(() => {
+    getOrders();
+    getFoodItems();
+    getFoodDetails();
+    getIngredients();
+    getFoodItemIngredients();
+  }, []);
 
-    return (
-        <div className="orders">
-        {
-            getFoodItems.map(foodItemObject => <OrderFoodItem key={foodItemObject.id} foodItemObject={foodItemObject} deleteOrder={deleteOrder} foodDetails={foodDetails} />)
-        }
-        </div>
-    )
-}
+  return (
+    <div className="orders">
+      {getFoodItems.map((foodItemObject) => (
+        <OrderFoodItem
+          key={foodItemObject.id}
+          foodItemObject={foodItemObject}
+          deleteOrder={deleteOrder}
+          foodDetails={foodDetails}
+          ingredients={ingredients}
+          foodItemIngredients={foodItemIngredients}
+        />
+      ))}
+    </div>
+  );
+};
